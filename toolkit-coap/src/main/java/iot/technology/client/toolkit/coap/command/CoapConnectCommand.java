e03904e0360646c8f3ac8ca7603781c2e179503c
sanshengshui <lovewsic@gmail.com>
2022-05-19 22:24:17 +0800

    process program

--- toolkit-coap/src/main/java/iot/technology/client/toolkit/coap/command/CoapConnectCommand.java
+++ toolkit-coap/src/main/java/iot/technology/client/toolkit/coap/command/CoapConnectCommand.java
@@ -9,18 +9,24 @@ import java.util.concurrent.Callable;
  * @author mushuwei
  */
 @Component
-@CommandLine.Command
+@CommandLine.Command(name = "echoo", mixinStandardHelpOptions = true, version = "echoo 1.0",
+		description = "Echo back a message to STDOUT.")
 public class CoapConnectCommand implements Callable<Integer> {
 
-	@CommandLine.Option(names = {"-h", "--hostname"}, description = "The URI of the server to connect to.")
-	private String uri;
+	@CommandLine.Option(names = {"-c", "--capitalize"}, description = "To capitalize (true, false)")
+	private boolean capitalize = true;
 
-	@CommandLine.Parameters(index = "0", description = "")
-	private String host;
+	@CommandLine.Parameters(index = "0", description = "A message to echo back.")
+	private String message;
 
 
 	@Override
-	public Integer call() throws Exception {
-		return null;
+	public Integer call() {
+		if (capitalize) {
+			System.out.println("message: " + message.toUpperCase());
+		} else {
+			System.out.println("message: " + message.toLowerCase());
+		}
+		return 0;
 	}
 }
