6fbba74451817934e4e51eb6d01c0f7ed3a3d813
sanshengshui <lovewsic@gmail.com>
2023-02-14 22:16:01 +0800

    improve nb help info

--- toolkit-nbiot/src/main/java/iot/technology/client/toolkit/nb/service/processor/telecom/TelHelpProcessor.java
+++ toolkit-nbiot/src/main/java/iot/technology/client/toolkit/nb/service/processor/telecom/TelHelpProcessor.java
@@ -66,7 +66,7 @@ public class TelHelpProcessor implements TkProcessor {
 		sb.append(ColorUtils.cyanAnnotation("update:  " + bundle.getString("nb.operation.update.desc")))
 				.append(StringUtils.lineSeparator());
 		sb.append("    usage: update imei name").append(StringUtils.lineSeparator());
-		System.out.format(sb.toString());
+		sb.append(StringUtils.lineSeparator());
 
 		// print telecom nb-iot device reported data
 		sb.append(ColorUtils.cyanAnnotation("log:      " + bundle.getString("nb.operation.log.desc")))
