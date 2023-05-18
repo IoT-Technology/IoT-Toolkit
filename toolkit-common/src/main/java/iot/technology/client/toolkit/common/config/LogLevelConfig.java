1f18a04ca00092500059c55ef3e2ca79e3b94705
sanshengshui <lovewsic@gmail.com>
2023-05-18 10:17:36 +0800

    add slf4j simple jar

--- toolkit-common/src/main/java/iot/technology/client/toolkit/common/config/LogLevelConfig.java
+++ toolkit-common/src/main/java/iot/technology/client/toolkit/common/config/LogLevelConfig.java
@@ -22,6 +22,7 @@ public class LogLevelConfig {
 
 	// log level
 	public static void setLogLevel() {
+		System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "ERROR");
 		System.setProperty("org.slf4j.simpleLogger.log.org.eclipse.californium", "ERROR");
 		System.setProperty("org.slf4j.simpleLogger.log.org.eclipse.californium.scandium", "ERROR");
 	}
