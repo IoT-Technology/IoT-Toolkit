1d9b3d012410fc1d98b71637bcc5b2685b68948c
sanshengshui <lovewsic@gmail.com>
2025-02-18 20:49:29 +0800

    add onenet config

---
+++ toolkit-common/src/main/java/iot/technology/client/toolkit/common/constants/OneNetSettings.java
@@ -0,0 +1,11 @@
+package iot.technology.client.toolkit.common.constants;
+
+
+public interface OneNetSettings {
+
+    String ONENET_AUTH_VERSION = "2022-05-01";
+    String ONENET_AUTH_RES_PREFIX = "products/";
+    String ONENET_AUTH_METHOD = "sha256";
+    String ONENET_AUTH_HEADER = "Authorization";
+
+}
