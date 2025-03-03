c795f32b80a5c329a5b90802861b658e16a2ffd8
sanshengshui <lovewsic@gmail.com>
2025-03-03 20:04:28 +0800

    add OneNetDelDeviceProcessor class

--- toolkit-nbiot/src/main/java/iot/technology/client/toolkit/nb/service/processor/mobile/OneNetDelDeviceProcessor.java
+++ toolkit-nbiot/src/main/java/iot/technology/client/toolkit/nb/service/processor/mobile/OneNetDelDeviceProcessor.java
@@ -2,8 +2,19 @@ package iot.technology.client.toolkit.nb.service.processor.mobile;
 
 import iot.technology.client.toolkit.common.rule.ProcessContext;
 import iot.technology.client.toolkit.common.rule.TkAbstractProcessor;
+import iot.technology.client.toolkit.common.utils.ColorUtils;
+import iot.technology.client.toolkit.common.utils.StringUtils;
+import iot.technology.client.toolkit.nb.service.mobile.OneNetDeviceService;
+import iot.technology.client.toolkit.nb.service.mobile.domain.MobileConfigDomain;
+import iot.technology.client.toolkit.nb.service.mobile.domain.action.device.OneNetDelDeviceRequest;
+import iot.technology.client.toolkit.nb.service.mobile.domain.action.device.OneNetDelDeviceResponse;
+import iot.technology.client.toolkit.nb.service.processor.MobProcessContext;
+
+import java.util.List;
 
 public class OneNetDelDeviceProcessor extends TkAbstractProcessor {
+
+    private final OneNetDeviceService oneNetDeviceService = new OneNetDeviceService();
 
 
     @Override
@@ -13,7 +24,26 @@ public class OneNetDelDeviceProcessor extends TkAbstractProcessor {
 
     @Override
     public void handle(ProcessContext context) {
-
-
+        List<String> arguArgs = List.of(context.getData().split(" "));
+        if (arguArgs.size() != 2) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(ColorUtils.redError("imei is required")).append(StringUtils.lineSeparator);
+            sb.append(ColorUtils.blackBold("detail usage please enter: help del"));
+            System.out.println(sb);
+            return;
+        }
+        String imei = arguArgs.get(1);
+        MobProcessContext mobProcessContext = (MobProcessContext) context;
+        MobileConfigDomain mobileConfigDomain = mobProcessContext.getMobileConfigDomain();
+        OneNetDelDeviceRequest request = new OneNetDelDeviceRequest();
+        request.setProductId(mobileConfigDomain.getProductId());
+        request.setImei(imei);
+        OneNetDelDeviceResponse response = oneNetDeviceService.delete(mobileConfigDomain, request);
+        if (response.isSuccess()) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(String.format(ColorUtils.blackBold("imei:%s delete success"), imei));
+            sb.append(StringUtils.lineSeparator());
+            System.out.println(sb);
+        }
     }
 }
